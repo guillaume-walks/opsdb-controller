ee1cfaee879e1cd1ca0fe25b3cc16ce710e5d880
Guillaume Ader <guillaume@Guillaumes-MacBook-Pro-2.local>
2019-03-09 13:45:14 +0000

    init

---
+++ src/components/loadData.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1 class="title">LOAD DATA widget '{{ title }}'</h1>
+    <div>
+      <ul>
+        <li v-for="(val, index) in obj" :key="val">{{index}}:: {{val}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "opsdbComponent",
+  mounted() {
+    console.log(this);
+  },
+  props: {
+    title: String,
+    obj: Object
+  }
+};
+</script>
+
+<style scoped>
+* {
+  font-family: "Courier New", Courier, monospace;
+  font-weight: 600;
+  text-align: center;
+  list-style: none;
+}
+.title {
+}
+ul {
+  font-weight: 300;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
-import App from './App.vue'
+import LoadData from './components/loadData'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(LoadData, {
+    props: {
+      title: 'Monsieur',
+      obj: {
+        message: 'helloo you',
+        url: 'http://google.com'
+      }
+    }
+  }),
 }).$mount('#app')
